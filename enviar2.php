41b5b3e62da80ea9ea9b2b8006d70e392779d488
FamilyFlow-Maternidad <nagoreabad@gmail.com>
2026-04-24 07:48:28 +0200

    Update enviar2.php
    Incorporación del .php para el formulario

--- enviar2.php
+++ enviar2.php
@@ -13,8 +13,8 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'info@puertomarinaboats.es';
-$asunto = 'Mensaje de mi sitio web';
+$para = 'nagoreabad@gmail.com';
+$asunto = 'Mensaje de FamilyFlow';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
